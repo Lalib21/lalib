6b5ca0810a2ce95a69f0897d9e8374569f7ac1e6
Lalib21 <123494440+Lalib21@users.noreply.github.com>
2023-01-26 10:09:01 +0100

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'f9b907efc1f24e7deea958ae926f373d',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://xhzz3moj1dsd.com/f9b907efc1f24e7deea958ae926f373d/invoke.js"></scr' + 'ipt>');
+</script>
